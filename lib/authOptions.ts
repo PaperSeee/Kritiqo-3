b6d0f0bfca4021c5191be68286820a7a0e1bc86c
paperseee <ilias300@outlook.be>
2025-05-25 18:17:44 +0200

    feat: Update Azure AD provider configuration to use common tenant ID

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -16,7 +16,7 @@ export const authOptions: NextAuthOptions = {
     AzureADProvider({
       clientId: process.env.AZURE_AD_CLIENT_ID!,
       clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
-      tenantId: process.env.AZURE_AD_TENANT_ID!,
+      tenantId: "common",
       authorization: {
         params: {
           scope: "openid profile email offline_access https://graph.microsoft.com/Mail.Read"
